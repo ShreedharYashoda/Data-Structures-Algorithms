aaaba5492b057a4b186774b07a2a47a6f24dc523
Shreedhar Sampath Kumar <43503503+ShreedharYashoda@users.noreply.github.com>
2021-07-05 22:29:26 +0000

    Added Can Construct String from vocabulary.

---
+++ Recursion + DP/CanConstruct.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class CanConstruct {
+	
+  public static void main(String[] args) {
+		Map<String, Boolean> cache = new HashMap<>();
+    System.out.println(canConstruct("abcdef", Arrays.asList("abc", "cd", "def", "abcd"), cache));
+
+		cache = new HashMap<>();
+		System.out.println(canConstruct("skateboard", Arrays.asList("bo","rd","ate","t","ska","sk","boar"), cache));
+
+		cache = new HashMap<>();
+		System.out.println(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", Arrays.asList("e","ee","eee","eeee","eeee"), cache));
+		
+  }
+
+	public static boolean canConstruct(String target, List<String> dict, Map<String, Boolean> cache){
+			if(cache.containsKey(target)) return cache.get(target);
+			if(target.equals("")) return true;
+			
+			for(String str:dict){
+				if(target.indexOf(str) == 0){
+					String sub = target.substring(str.length());
+					if(canConstruct(sub, dict, cache)){
+						cache.put(target, true);
+						return true;
+					}
+				}
+			}
+
+			cache.put(target, false);
+			return false;
+	}
+}
