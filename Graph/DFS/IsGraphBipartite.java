dbb4b28c6cf7058c6220327e1aa79021dc7619ea
Shreedhar Sampath Kumar <43503503+ShreedharYashoda@users.noreply.github.com>
2021-07-11 18:15:49 +0000

    Added iterative method

--- Graph/DFS/IsGraphBipartite.java
+++ Graph/DFS/IsGraphBipartite.java
@@ -32,10 +32,42 @@ class IsGraphBipartite {
         return isBip;
     }
 
-		public static void main(String[] args){
-			// [[1,2,3],[0,2],[0,1,3],[0,2]]
-			System.out.println(isBipartite(new int[][]{{1,2,3},{0,2},{0,1,3},{0,2}}));
+    public static boolean iterative(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int start=0; start<n; start++){
+            if(color[start] == -1){
+                color[start] = 0;
+                stack.add(start);
+                
+                while(!stack.isEmpty()){
+                    int node = stack.pop();
+                    for(int nbr:graph[node]){
+                        if(color[nbr] == -1){
+                            color[nbr] = color[node] ^ 1;
+                            stack.add(nbr);
+                        }else if(color[node] == color[nbr]){
+                            return false;
+                        }
+                        
+                    }
+                }
+            }
+        }
+        
+        return true;
 
+		}
+
+		public static void main(String[] args){
+			System.out.println(isBipartite(new int[][]{{1,2,3},{0,2},{0,1,3},{0,2}}));
 			System.out.println(isBipartite(new int[][]{{1,3},{0,2},{1,3},{0,2}}));
+
+			System.out.println(iterative(new int[][]{{1,2,3},{0,2},{0,1,3},{0,2}}));
+			System.out.println(iterative(new int[][]{{1,3},{0,2},{1,3},{0,2}}));
 		}
 }
