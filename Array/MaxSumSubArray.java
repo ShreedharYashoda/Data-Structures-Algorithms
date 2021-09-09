7188c7a72beee9feb8047b257723c1f07dd43317
Shreedhar Sampath Kumar <43503503+ShreedharYashoda@users.noreply.github.com>
2021-09-09 01:36:25 +0000

    Kadane's negative number

--- Array/MaxSumSubArray.java
+++ Array/MaxSumSubArray.java
@@ -1,14 +1,26 @@
 class MaxSumSubArray {
 
+	// O(n) Doesn't handle all negative array.
 	public static int maxSumSubArray(int[] A) {
 		int maxSoFar = 0;
 		int maxEndingHere = 0;
 
 		for(int i:A){
 			maxEndingHere += i;
-
 			maxEndingHere = Integer.max(maxEndingHere, 0);
+			maxSoFar = Integer.max(maxSoFar, maxEndingHere);
+		}
 
+		return maxSoFar;
+	}
+
+	public static int maxSumSubArray2(int[] A) {
+		int maxSoFar = Integer.MIN_VALUE;
+		int maxEndingHere = 0;
+
+		for(int i:A){
+			maxEndingHere += i;
+			maxEndingHere = Integer.max(maxEndingHere, i);
 			maxSoFar = Integer.max(maxSoFar, maxEndingHere);
 		}
 
@@ -17,7 +29,10 @@ class MaxSumSubArray {
 
 	public static void main(String[] args){
 		int[] A = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+		System.out.println("MaxSumSubArray is " + maxSumSubArray2(A));
 
-		System.out.println("MaxSumSubArray is " + maxSumSubArray(A));
+		int[] B = { -8, -3, -6, -2, -5, -4 };
+		System.out.println("MaxSumSubArray is " + maxSumSubArray2(B));
+
 	}
 }
